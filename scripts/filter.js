9ec95e06f1d538e58c36d3eb45c76027e0650ce5
Juan Espinosa <juancamiloespinosab@gmail.com>
2021-05-18 16:46:58 -0500

    Se agrega funcion de buscar al pulsar ENTER

--- scripts/filter.js
+++ scripts/filter.js
@@ -3,7 +3,7 @@ class Filter {
     this.searchState = 'closed';
     this.searchBox = document.getElementById('main-menu__search-box');
     this.mainMenu = document.getElementById('main-menu');
-    this.grid = document.getElementById('grid');
+    this.projects = document.getElementById('projects');
     this.search;
 
     this.filters = document.getElementById(filterId);
@@ -25,36 +25,51 @@ class Filter {
         this.search = e.target;
 
         if (this.searchState == 'closed') {
-          this.search.classList.add('main-menu__search--active');
-          this.searchBox.classList.add('main-menu__search-box--active');
-          this.mainMenu.classList.add('main-menu--search-active');
-          this.searchState = 'open';
-          this.search.setAttribute('href', '#');
+          this.openSearch();
         } else {
-          // this.search.setAttribute('href', '#projects');
-          this.searchBox.classList.remove('main-menu__search-box--active');
-          this.search.classList.remove('main-menu__search--active');
-          this.mainMenu.classList.remove('main-menu--search-active');
-          this.searchState = 'closed';
-          menu.closeMenu();
-
-          scroll({
-            top: this.grid.offsetTop,
-            behavior: "smooth"
-          });
-
-
-          this.removeItemActive();
-          this.addItemActive('All');
-
-          cards.clear();
-          cards.setCounters();
-          cards.renderCardsByTitle(this.searchBox.value);
-
+          this.closeSearch();
         }
+
+        this.searchBox.addEventListener('keyup', e => {
+          if (e.code == 'Enter' || e.key == 'Enter') {
+            this.closeSearch()
+          }
+        })
 
       }
 
+    })
+  }
+
+  openSearch() {
+    this.search.classList.add('main-menu__search--active');
+    this.searchBox.classList.add('main-menu__search-box--active');
+    this.mainMenu.classList.add('main-menu--search-active');
+    this.searchState = 'open';
+    this.search.setAttribute('href', '#');
+  }
+
+  closeSearch() {
+    this.searchBox.classList.remove('main-menu__search-box--active');
+    this.search.classList.remove('main-menu__search--active');
+    this.mainMenu.classList.remove('main-menu--search-active');
+    this.searchState = 'closed';
+    menu.closeMenu();
+
+    this.scrollToGrid();
+
+    this.removeItemActive();
+    this.addItemActive('All');
+
+    cards.clear();
+    cards.setCounters();
+    cards.renderCardsByTitle(this.searchBox.value);
+  }
+
+  scrollToGrid() {
+    window.scroll({
+      top: this.projects.offsetTop,
+      behavior: "smooth"
     })
   }
 
